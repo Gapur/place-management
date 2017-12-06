1798b52248f5ed28cb178c02cde9af89cb39cc6b
Gapur Kassym <gapur.kassym@gmail.com>
2017-12-06 16:35:36 +0600

    place form fields

--- src/places/components/PlaceForm.js
+++ src/places/components/PlaceForm.js
@@ -2,24 +2,32 @@ import React, { Component } from 'react';
 import { Form, Button, Icon, Row, Col } from 'antd';
 import { Field, reduxForm } from 'redux-form';
 import { Link } from 'react-router-dom';
+import { compose } from 'redux';
+import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps";
 import {
   renderInput,
   renderSelect,
   renderLabel,
+  renderTextarea,
+  renderInputUpload,
+  renderPlacesAutocomplete,
 } from '../../shared/utils/form_components';
 import { required } from '../../shared/utils/form_validations';
 
 const FormItem = Form.Item;
 const options = ['Thailand', 'Qazakhstan', 'Japan'];
 
-const tailFormItemLayout = {
-  wrapperCol: {
-    sm: {
-      span: 16,
-      offset: 8,
-    },
-  },
-};
+const PlaceMap = compose(
+  withScriptjs,
+  withGoogleMap,
+)(({ input: { onChange, value }, isMarkerShown }) => (
+  <GoogleMap
+    defaultZoom={4}
+    defaultCenter={{ lat: -34.397, lng: 150.6449 }}
+  >
+    {isMarkerShown && <Marker position={{ lat: -34.397, lng: 150.644 }} />}
+  </GoogleMap>
+));
 
 class PlaceForm extends Component {
   render() {
@@ -27,14 +35,70 @@ class PlaceForm extends Component {
 
     return (
       <Form onSubmit={handleSubmit}>
+        <Row>
+          <div className="is-right">
+            <FormItem>
+              <Button style={{ marginRight: 5 }}>
+                <Link to="/places">Cancel</Link>
+              </Button>
+              <Button type="primary" htmlType="submit">
+                <Icon type="save" />Save
+              </Button>
+            </FormItem>
+          </div>
+        </Row>
         <Row gutter={32}>
-          <Col span={8} offset={2}>
+          <Col span={8}>
             <Field
               name="place_name"
               label="Place Name"
               component={renderInput}
               placeholder="Place Name"
               validate={required}
+            />
+
+            <Field
+              name="description"
+              label="Description"
+              component={renderTextarea}
+              placeholder="Description"
+              validate={required}
+            />
+
+            <Field
+              name="address"
+              label="Address"
+              component={renderInput}
+              placeholder="Address Number"
+              validate={required}
+            />
+
+            <Field
+              name="street"
+              label="Street"
+              component={renderInput}
+              placeholder="Street / Route"
+            />
+
+            <Field
+              name="arrea_or_district"
+              label="Arrea / District"
+              component={renderInput}
+              placeholder="City Arrea / District"
+            />
+
+            <Field
+              name="city_town"
+              label="City / Town"
+              component={renderInput}
+              placeholder="City / Town"
+            />
+
+            <Field
+              name="state"
+              label="State / Province"
+              component={renderInput}
+              placeholder="State / Province"
             />
 
             <Field
@@ -47,19 +111,63 @@ class PlaceForm extends Component {
             />
 
             <Field
-              name="city"
-              label="City"
+              name="placeId"
+              label="Place ID"
               component={renderInput}
-              placeholder="City"
+              placeholder="@12x3jk9"
               validate={required}
             />
+          </Col>
+
+          <Col span={8}>
+            <Row>
+              <Col span={8}>
+                <span>Location:</span>
+              </Col>
+              <Col span={8}>
+                <Field
+                  name="lat"
+                  component={renderInput}
+                  placeholder="lat"
+                  validate={required}
+                />
+              </Col>
+              <Col span={8}>
+                <Field
+                  name="long"
+                  component={renderInput}
+                  placeholder="long"
+                  validate={required}
+                />
+              </Col>
+            </Row>
+
+            <FormItem>
+              <Field
+                name="place_loc"
+                component={PlaceMap}
+                isMarkerShown
+                googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places"
+                loadingElement={<div style={{ height: `100%` }} />}
+                containerElement={<div style={{ height: `300px` }} />}
+                mapElement={<div style={{ height: `100%` }} />}
+              />
+            </FormItem>
 
             <Field
               name="source"
               label="Source"
-              component={renderInput}
-              placeholder="google"
-              validate={required}
+              component={renderPlacesAutocomplete}
+              placeholder="Google Place"
+            />
+          </Col>
+
+          <Col span={8}>
+            <Field
+              name="profile_picture"
+              label="Profile Picture"
+              component={renderInputUpload}
+              placeholder="Upload Picture"
             />
 
             <Field
@@ -73,19 +181,10 @@ class PlaceForm extends Component {
               label="Create by"
               component={renderLabel}
             />
-
-            <FormItem {...tailFormItemLayout}>
-              <Button style={{ marginRight: 5 }}>
-                <Link to="/places">Cancel</Link>
-              </Button>
-              <Button type="primary" htmlType="submit">
-                <Icon type="save" />Save
-              </Button>
-            </FormItem>
           </Col>
         </Row>
       </Form>
-    )
+    );
   }
 }
 
--- src/shared/utils/form_components.js
+++ src/shared/utils/form_components.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Form, Input, Select, Checkbox, DatePicker, Upload, Icon } from 'antd';
+import PlacesAutocomplete from 'react-places-autocomplete';
 
 const FormItem = Form.Item;
 const Option = Select.Option;
@@ -21,9 +22,9 @@ export function getValidationState({ error, warning, touched }) {
 }
 
 export const renderInput = (props) => {
-  const { input, label, meta } = props;
+  const { input, label, meta, placeholder } = props;
   const id = `${meta.form}_${input.name}`;
-  const inputProps = { ...input, id: id };
+  const inputProps = { ...input, id: id, placeholder };
   const validateStatus = getValidationState(meta);
 
   return (
@@ -77,7 +78,7 @@ export const renderDateTime = (props) => {
       <DatePicker {...inputProps} />
     </FormItem>
   );
-}
+};
 
 export const renderLabel = (props) => {
   const { input, label, meta } = props;
@@ -89,12 +90,12 @@ export const renderLabel = (props) => {
       <span {...inputProps}>{input.value}</span>
     </FormItem>
   );
-}
+};
 
 export const renderTextarea = (props) => {
-  const { input, label, meta } = props;
+  const { input, label, meta, placeholder } = props;
   const id = `${meta.form}_${input.name}`;
-  const inputProps = { ...input, id: id };
+  const inputProps = { ...input, id: id, placeholder };
   const validateStatus = getValidationState(meta);
 
   return (
@@ -104,15 +105,15 @@ export const renderTextarea = (props) => {
       validateStatus={validateStatus}
       help={meta.touched && meta.error && meta.error || ''}
     >
-      <textarea style={{ width: '100%' }} {...inputProps} />
+      <textarea className="ant-input" style={{ width: '100%' }} {...inputProps} />
     </FormItem>
   );
 };
 
 export const renderInputUpload = (props) => {
-  const { input, label, meta } = props;
+  const { input, label, meta, ...custom } = props;
   const id = `${meta.form}_${input.name}`;
-  const inputProps = { ...input, id: id };
+  const inputProps = { ...input, id: id, ...custom };
   const validateStatus = getValidationState(meta);
   const uploadButton = (
     <div>
@@ -130,10 +131,37 @@ export const renderInputUpload = (props) => {
     >
       <Upload {...inputProps}
         listType="picture-card"
-        showUploadList={false}
       >
         {input.value ? <img src={input.value.file} alt="image" /> : uploadButton}
       </Upload>
+    </FormItem>
+  );
+};
+
+export const renderPlacesAutocomplete = (props) => {
+  const { input, label, meta, placeholder } = props;
+  const id = `${meta.form}_${input.name}`;
+  const inputProps = { ...input, id: id, placeholder };
+  const validateStatus = getValidationState(meta);
+
+  return (
+    <FormItem
+      {...formItemLayout}
+      label={label ? label : ''}
+      validateStatus={validateStatus}
+      help={meta.touched && meta.error && meta.error || ''}
+    >
+      <PlacesAutocomplete
+        inputProps={inputProps}
+        classNames={{
+          input: "ant-input",
+        }}
+        styles={{
+          autocompleteContainer: {
+            zIndex: 2,
+          }
+        }}
+      />
     </FormItem>
   );
 };
