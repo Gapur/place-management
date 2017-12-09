a1bae44b32511c57ed8e25a33e0957aec2079e06
Gapur Kassym <gapur.kassym@gmail.com>
2017-12-09 11:36:56 +0600

    unit testing renders users and places

--- src/__tests__/stories/CustomTags.test.js
+++ src/__tests__/stories/CustomTags.test.js
@@ -5,31 +5,40 @@ import CustomTags from '../../stories/components/CustomTags';
 describe('testing custom tags', () => {
   it('renders CustomTags is creating', () => {
     const changeTags = jest.fn();
+    const updateTags = jest.fn();
     const customTagsTest = shallow(
       <CustomTags
         tags={['tags']}
         isCreating={true}
         onChange={changeTags}
-        onUpdate={jest.fn()}
+        onUpdate={updateTags}
         onDelete={jest.fn()}
         onClick={jest.fn()}
       />
     );
-
-    customTagsTest.find('input').simulate('change', { target: { value: 'Test' } });
-    expect(changeUsernameSpy).toBeCalledWith('Test');
+    
+    customTagsTest.find('Input').simulate('change', 'Test');
+    expect(changeTags).toBeCalledWith('Test');
+    expect(customTagsTest.find('Tag').length).toBe(1);
+    customTagsTest.find('Input').simulate('blur');
+    expect(updateTags).toBeCalledWith();
   });
 
   it('renders CustomTags is not creating', () => {
+    const changeTags = jest.fn();
+    const updateTags = jest.fn();
     const customTagsTest = shallow(
       <CustomTags
         tags={['tags']}
         isCreating={false}
-        onChange={jest.fn()}
-        onUpdate={jest.fn()}
+        onChange={changeTags}
+        onUpdate={updateTags}
         onDelete={jest.fn()}
         onClick={jest.fn()}
       />
     );
+    console.log(customTagsTest.debug());
+    expect(customTagsTest.find('Input').length).toBe(0);
+    expect(customTagsTest.find('Tag').length).toBe(2);
   });
 });
