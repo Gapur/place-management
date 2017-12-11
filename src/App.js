a041338ff0c0bf3add4c067ee46304ab8f48f58c
Gapur Kassym <gapur.kassym@gmail.com>
2017-12-11 23:29:14 +0600

    init graphql server

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 import { ConnectedRouter } from 'react-router-redux';
+import { ApolloClient } from 'apollo-client';
+import { HttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
 
 import { store, history } from './store';
 import AppRouter from './Router';
